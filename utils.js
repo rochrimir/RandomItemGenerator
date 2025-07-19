0af9ade448dd3b22209bcc509de04bfa889bdfea
Rod <rodolfchristianmiranda@gmail.com>
2025-07-19 19:48:30 +0800

    utils update v1

--- utils.js
+++ utils.js
@@ -1,9 +1,14 @@
-function getRandomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
 
-function getRandomInt(min, max) {
+// utils.js – shared utility functions
+
+export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomFromArray, getRandomInt };
+export function getRandomFloat(min, max) {
+  return Math.round((Math.random() * (max - min) + min) * 100) / 100;
+}
+
+export function pickRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
